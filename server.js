ea5725a29ad2ad1cb49c3a9c3751131ddfd8a5df
the-wtag <tanvir.hemel@welldev.io>
2021-11-28 19:01:14 +0600

    Add authentication test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
+let corsOptions = {
     origin: "http://localhost:8081"
 };
 
@@ -17,8 +17,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
+const Role = db.role;
+
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
+    initial();
 });
 
 // simple route
@@ -26,10 +29,30 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
 });
 
+// routes
 require("./app/routes/turorial.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
